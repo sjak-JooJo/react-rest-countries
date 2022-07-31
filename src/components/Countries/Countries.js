694c6472342f40f8d07da9227214256df06ca230
JooJo <sjak0101@gmail.com>
2022-07-31 03:24:32 +0100

    commit agin

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Country from '../Country/Country';
 
 const Countries = () => {
+    const [countries, setCountreis] = useState([]);
+
+    useEffect( ()=>{
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => setCountreis(data))
+    } , [])
+
     return (
         <div>
-            <h2>Countries in other File.</h2>
+            <h1>Hello From Countreis: {countries.length}</h1>
+            {
+                countries.map(country => <Country 
+                    name={country.name.common} 
+                    population={country.population}
+                    area={country.area}></Country>)
+            }
         </div>
     );
 };
